b9e0b85836c9fada908df2c8122c84b2cec02f52
MaherKouki <koukimaher386@gmail.com>
2025-01-22 03:13:01 +0100

    commit

--- messagingapp/src/main/java/com/example/messagingapp/chat/ChatController.java
+++ messagingapp/src/main/java/com/example/messagingapp/chat/ChatController.java
@@ -1,4 +1,32 @@
 package com.example.messagingapp.chat;
 
+
+import com.example.messagingapp.common.StringResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
+
+
+    @PostMapping
+    public ResponseEntity <StringResponse> createdChat(
+            @RequestParam(name = "sender-Id") String senderId,
+            @RequestParam(name = "receiver-Id") String receiverId){
+
+        final String chatId = chatService.createChat(senderId,receiverId);
+
+        StringResponse stringResponse = StringResponse.builder()
+                .response(chatId)
+                .build();
+        return ResponseEntity.ok(stringResponse);
+    }
 }
---
+++ messagingapp/src/main/java/com/example/messagingapp/common/StringResponse.java
@@ -0,0 +1,15 @@
+package com.example.messagingapp.common;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+
+
+    private String response;
+}
