692d7ffbabc3fb3eea55f638549adc6711114ca7
MaherKouki <koukimaher386@gmail.com>
2025-01-25 02:35:29 +0100

    implementing ui

--- messaging-app-ui/src/app/components/chat-list/chat-list.component.ts
+++ messaging-app-ui/src/app/components/chat-list/chat-list.component.ts
@@ -1,14 +1,32 @@
 import {Component, input, InputSignal} from '@angular/core';
 import {ChatResponse} from "../../services/models/chat-response";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.scss'
 })
 export class ChatListComponent {
 
   chats : InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
+  searchNewContact = false;
 
+  searchContact() {
+
+  }
+
+  chatClicked(chat: ChatResponse) {
+
+  }
+
+  wrapMessage(lastMessage: string | undefined):string {
+    if(lastMessage && lastMessage.length <= 20) {
+      return lastMessage;
+    }
+    return lastMessage?.substring(0,17) + '...';
+  }
 }
